c6c3396e5f6fec5c1d63388edff397bb5af9c9d6
paulammoun <paul@pcs-lb.com>
2025-04-13 19:13:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // **Template code last revised: 08/30/21
 
-const staticCacheName = 'test Amiga-Cloud2_pwa_cache-v-01.59';
-const dynamicCacheName = 'test Amiga-Cloud2_pwa_dynamic_cache-v-01.59';
+const staticCacheName = 'test Amiga-Cloud2_pwa_cache-v-01.60';
+const dynamicCacheName = 'test Amiga-Cloud2_pwa_dynamic_cache-v-01.60';
 const dynamicCacheSize = 10;
 const offlineFilename = './offline.html';
 const showDebugInfo = false;
